e833e1947a41dff710f1d62a5b6b18abc964f080
franzrupal <franzadrian.rupal.20@usjr.edu.ph>
2022-12-24 20:35:46 +0800

    Changes in Cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import { ClothingShopContext, } from "../useContext/shopContext";
 import { Product } from "../../models";
 import { ProductCard } from "../ProductCard";
-import { ProductsWrapper, Title } from "./Cart.styled";
+import { ProductsWrap, ProductsWrapper, Title } from "./Cart.styled";
+import { Quantity } from "../Quantity";
 
 export const Cart = () => {
   const { products, total } = useContext(ClothingShopContext);
@@ -11,7 +12,7 @@ export const Cart = () => {
       <Title>Your cart total is {total}.00$</Title>
       <ProductsWrapper>
         {products.map((product: Product, index) => (
-          <ProductCard {...product} key={index} />
+          <ProductsWrap><ProductCard {...product} key={index} /><Quantity {...product} key={index}></Quantity></ProductsWrap>
         ))}
       </ProductsWrapper>
     </>
